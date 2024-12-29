13b1a9beb42400ce71ece92808a152317ae78217
fakeistiak <istiakahmedb100.com>
2024-12-29 23:44:37 +0600

    added forget password

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -4,26 +4,26 @@ import {
   getAuth,
   GithubAuthProvider,
   GoogleAuthProvider,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
   signInWithPopup,
   signOut,
 } from "firebase/auth";
 import app from "@/firebase/firebase.init";
-import { useState } from "react";
-import { FaGithub } from "react-icons/fa";
+import { useRef, useState } from "react";
+import { FaEye, FaEyeSlash, FaGithub } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { RiLockPasswordFill } from "react-icons/ri";
 
 const Login = () => {
   const [user, setUser] = useState(null);
+  const [showPassword, setShowPassword] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
   const auth = getAuth(app);
+  const emailRef = useRef();
   const googleProvider = new GoogleAuthProvider();
   const githubProvider = new GithubAuthProvider();
 
-  const handleRegister=e=>{
-    e.preventDefault();
-    const email=e.target.email.value;
-    const password=e.target.password.value;
-    console.log(email,password)
-  }
-  //Login With Github
   const handleGoogleSignin = () => {
     signInWithPopup(auth, googleProvider)
       .then((result) => {
@@ -31,77 +31,125 @@ const Login = () => {
         setUser(loggedInUser);
       })
       .catch((error) => {
-        console.log("error", error.message);
+        setErrorMessage(error.message);
       });
   };
 
-  //Login With Github
   const handleGithubSignin = () => {
-    signInWithPopup(auth, githubProvider).then((result) => {
-      const loggedUser = result.user;
-      console.log(loggedUser);
-      setUser(loggedUser);
-    });
+    signInWithPopup(auth, githubProvider)
+      .then((result) => {
+        const loggedUser = result.user;
+        setUser(loggedUser);
+      })
+      .catch((error) => {
+        setErrorMessage(error.message);
+      });
   };
 
-  //SignOut
   const handleSignOut = () => {
     signOut(auth)
       .then(() => {
         setUser(null);
       })
       .catch((error) => {
-        console.log("error", error.message);
+        setErrorMessage(error.message);
       });
   };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    setErrorMessage("");
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        setUser(result.user);
+      })
+      .catch((error) => {
+        setErrorMessage(error.message);
+      });
+  };
+
+  const handleForgetPassword = () => {
+    const email = emailRef.current.value;
+    if (!email) {
+      setErrorMessage("Please provide an email address.");
+      return;
+    }
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+      setErrorMessage("Please enter a valid email address.");
+      return;
+    }
+
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        setErrorMessage("Password reset email sent! Please check your inbox.");
+      })
+      .catch((error) => {
+        setErrorMessage(error.message);
+      });
+  };
+
   return (
     <div>
       <div className="min-h-screen flex items-center justify-center bg-gray-100">
         <div className="w-full max-w-md bg-white rounded-lg shadow-md p-8">
-          <h2 className="lg:text-4xl md:text-4xl text-3xl font-bold text-center mb-6 text-sky-600">Login</h2>
-        <h2 className="text-2xl font-bold text-center mb-6">
-          Email : {user ? user.displayName : "Guest"}
-        </h2>
-          <form onSubmit={handleRegister}>
+          <h2 className="lg:text-4xl md:text-4xl text-3xl font-bold text-center mb-6 text-sky-600">
+            Login Now
+          </h2>
+          {errorMessage && (
+            <p className="text-red-600 text-center mb-4">{errorMessage}</p>
+          )}
+          <form onSubmit={handleLogin}>
             <div className="space-y-4">
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium text-gray-700 mb-1"
-                >
-                  Email
-                </label>
+              <div className="flex justify-center items-center gap-2">
+                <MdEmail className="text-2xl text-sky-600" />
                 <input
                   id="email"
                   type="email"
                   name="email"
+                  ref={emailRef}
                   required
                   className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
                   placeholder="Enter your email"
                 />
               </div>
+              <div className="flex items-center gap-2">
+                <RiLockPasswordFill className="text-2xl text-sky-600" />
+                <div className="relative w-full">
+                  <input
+                    id="password"
+                    type={showPassword ? "text" : "password"}
+                    name="password"
+                    required
+                    className="w-full px-3 py-2 pr-10 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                    placeholder="Enter your password"
+                  />
+                  <span onClick={() => setShowPassword(!showPassword)}>
+                    {showPassword ? (
+                      <FaEyeSlash className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 cursor-pointer" />
+                    ) : (
+                      <FaEye className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 cursor-pointer" />
+                    )}
+                  </span>
+                </div>
+              </div>
               <div>
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium text-gray-700 mb-1"
+                <button
+                  type="button"
+                  onClick={handleForgetPassword}
+                  className="pl-8 text-sky-600 underline hover:text-sky-800"
                 >
-                  Password
-                </label>
-                <input
-                  id="password"
-                  type="password"
-                  name="password"
-                  required
-                  className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                  placeholder="Enter your password"
-                />
+                  Forgot Password?
+                </button>
               </div>
             </div>
             <div className="mt-6 space-y-4">
               {user ? (
                 <div className="flex justify-center">
                   <Button onClick={handleSignOut} variant="custom2">
-                    Signout
+                    Sign Out
                   </Button>
                 </div>
               ) : (
@@ -109,7 +157,6 @@ const Login = () => {
                   Sign In
                 </Button>
               )}
-
               <div className="relative">
                 <div className="absolute inset-0 flex items-center">
                   <div className="w-full border-t border-gray-300"></div>
@@ -118,24 +165,29 @@ const Login = () => {
                   <span className="px-2 bg-white text-gray-500">Or</span>
                 </div>
               </div>
-              <button
-                type="button"
-                onClick={handleGoogleSignin}
-                className="w-full gap-2 flex items-center justify-center bg-white hover:bg-gray-50 text-gray-700 font-bold py-2 px-4 rounded border border-gray-300 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
-              >
-                <FcGoogle className="lg:text-2xl text-xl" />
-                Sign in with Google
-              </button>
-              <button
-                type="button"
-                onClick={handleGithubSignin}
-                className="w-full gap-2 flex items-center justify-center bg-white hover:bg-gray-50 text-gray-700 font-bold py-2 px-4 rounded border border-gray-300 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
-              >
-                <FaGithub className="lg:text-2xl text-xl" />
-                Sign in with Github
-              </button>
+              <div className="flex justify-center items-center gap-4">
+                <button
+                  type="button"
+                  onClick={handleGoogleSignin}
+                  className="gap-2 flex items-center justify-center bg-white hover:bg-gray-200 text-black font-bold py-2 px-4 rounded border border-gray-300 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
+                >
+                  <FcGoogle className="lg:text-2xl text-xl" />
+                </button>
+                <button
+                  type="button"
+                  onClick={handleGithubSignin}
+                  className="gap-2 flex items-center justify-center bg-white hover:bg-gray-200 text-black font-bold py-2 px-4 rounded border border-gray-300 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
+                >
+                  <FaGithub className="lg:text-2xl text-xl" />
+                </button>
+              </div>
             </div>
-            <p className="py-4 text-lg text-center">New to JobFusion? <a className="text-sky-600" href="/register">SignUp</a> </p>
+            <p className="py-4 text-lg text-center">
+              New to JobFusion?{" "}
+              <a className="text-sky-600" href="/register">
+                Sign Up
+              </a>{" "}
+            </p>
           </form>
         </div>
       </div>
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,195 +1,198 @@
 import { Button } from "@/components/ui/button";
 import { FcGoogle } from "react-icons/fc";
+import { MdDriveFileRenameOutline, MdEmail } from "react-icons/md";
+import { RiLockPasswordFill } from "react-icons/ri";
 import {
-    createUserWithEmailAndPassword,
+  createUserWithEmailAndPassword,
   getAuth,
   GithubAuthProvider,
   GoogleAuthProvider,
+  sendEmailVerification,
   signInWithPopup,
   signOut,
 } from "firebase/auth";
 import app from "@/firebase/firebase.init";
 import { useState } from "react";
-import { FaGithub } from "react-icons/fa";
+import { FaGithub, FaEye, FaPhoneAlt, FaEyeSlash } from "react-icons/fa";
 
 const Register = () => {
+  const [user, setUser] = useState(null);
+  const [showPassword, setShowPassword] = useState(false);
+  const [registerError, setRegisterError] = useState("");
+  const auth = getAuth(app);
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
 
-    const [user, setUser] = useState(null);
-    const [registerError,setRegisterError]=useState('');
-    const auth = getAuth(app);
-    const googleProvider = new GoogleAuthProvider();
-    const githubProvider = new GithubAuthProvider();
-  
-    const handleRegister=e=>{
-      e.preventDefault();
-      const name=e.target.name.value;
-      const number=e.target.number.value;
-      const email=e.target.email.value;
-      const password=e.target.password.value;
-      console.log(email,password,name,number)
-      setRegisterError('');
-      createUserWithEmailAndPassword(auth,email,password)
-      .then(result=>{
-        console.log(result.user)
-      })
-      .catch(error=>{
-        console.error(error)
-        setRegisterError(error.message)
-      })
-    }
-    //Login With Github
-    const handleGoogleSignin = () => {
-      signInWithPopup(auth, googleProvider)
-        .then((result) => {
-          const loggedInUser = result.user;
-          setUser(loggedInUser);
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const number = e.target.number.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password, name, number);
+    setRegisterError("");
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result.user);
+        sendEmailVerification(result.user)
+        .then(()=>{
+            console.log('Please check your email and verify your account')
         })
-        .catch((error) => {
-          console.log("error", error.message);
-        });
-    };
-  
-    //Login With Github
-    const handleGithubSignin = () => {
-      signInWithPopup(auth, githubProvider).then((result) => {
-        const loggedUser = result.user;
-        console.log(loggedUser);
-        setUser(loggedUser);
+      })
+      .catch((error) => {
+        console.error(error);
+        setRegisterError(error.message);
       });
-    };
-  
-    //SignOut
-    const handleSignOut = () => {
-      signOut(auth)
-        .then(() => {
-          setUser(null);
-        })
-        .catch((error) => {
-          console.log("error", error.message);
-        });
-  }
-    return (
-        <div>
-            <div className="min-h-screen flex items-center justify-center bg-gray-100">
-                    <div className="w-full max-w-md bg-white rounded-lg shadow-md p-8">
-                    <h2 className="lg:text-4xl md:text-4xl text-3xl font-bold text-center mb-6 text-sky-600">Register</h2>
-                    <h2 className="text-2xl font-bold text-center mb-6">
-                      Email : {user ? user.displayName : "Guest"}
-                    </h2>
-                      <form onSubmit={handleRegister}>
-                        <div className="space-y-4">
-                          <div>
-                            <label
-                              htmlFor="name"
-                              className="block text-sm font-medium text-gray-700 mb-1"
-                            >
-                              Name
-                            </label>
-                            <input
-                              id="name"
-                              type="name"
-                              name="name"
-                              required
-                              className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                              placeholder="Enter your name"
-                            />
-                          </div>
-                          <div>
-                            <label
-                              htmlFor="number"
-                              className="block text-sm font-medium text-gray-700 mb-1"
-                            >
-                              Number
-                            </label>
-                            <input
-                              id="number"
-                              type="number"
-                              name="number"
-                              required
-                              className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                              placeholder="Enter your number"
-                            />
-                          </div>
-                          <div>
-                            <label
-                              htmlFor="email"
-                              className="block text-sm font-medium text-gray-700 mb-1"
-                            >
-                              Email
-                            </label>
-                            <input
-                              id="email"
-                              type="email"
-                              name="email"
-                              required
-                              className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                              placeholder="Enter your email"
-                            />
-                          </div>
-                          <div>
-                            <label
-                              htmlFor="password"
-                              className="block text-sm font-medium text-gray-700 mb-1"
-                            >
-                              Password
-                            </label>
-                            <input
-                              id="password"
-                              type="password"
-                              name="password"
-                              required
-                              className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                              placeholder="Enter your password"
-                            />
-                          </div>
-                        </div>
-                        <div className="mt-6 space-y-4">
-                          {user ? (
-                            <div className="flex justify-center">
-                              <Button onClick={handleSignOut} variant="custom2">
-                                Signout
-                              </Button>
-                            </div>
-                          ) : (
-                            <Button type="submit" variant="custom2">
-                              Sign Up
-                            </Button>
-                          )}
-            
-                          <div className="relative">
-                            <div className="absolute inset-0 flex items-center">
-                              <div className="w-full border-t border-gray-300"></div>
-                            </div>
-                            <div className="relative flex justify-center text-sm">
-                              <span className="px-2 bg-white text-gray-500">Or</span>
-                            </div>
-                          </div>
-                          <button
-                            type="button"
-                            onClick={handleGoogleSignin}
-                            className="w-full gap-2 flex items-center justify-center bg-white hover:bg-gray-50 text-gray-700 font-bold py-2 px-4 rounded border border-gray-300 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
-                          >
-                            <FcGoogle className="lg:text-2xl text-xl" />
-                            Sign up with Google
-                          </button>
-                          <button
-                            type="button"
-                            onClick={handleGithubSignin}
-                            className="w-full gap-2 flex items-center justify-center bg-white hover:bg-gray-50 text-gray-700 font-bold py-2 px-4 rounded border border-gray-300 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
-                          >
-                            <FaGithub className="lg:text-2xl text-xl" />
-                            Sign up with Github
-                          </button>
-                        </div>
-                        <p className="py-4 text-lg text-center">Already Have an account? <a className="text-sky-600" href="/login">SignIn</a> </p>
-                        </form>
-                        {
-                            registerError && <p className="text-red-600 text-center">{registerError}</p>
-                        }
-                    </div>
-                  </div>
+  };
+  //Login With Github
+  const handleGoogleSignin = () => {
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        const loggedInUser = result.user;
+        setUser(loggedInUser);
+      })
+      .catch((error) => {
+        console.log("error", error.message);
+      });
+  };
+
+  //Login With Github
+  const handleGithubSignin = () => {
+    signInWithPopup(auth, githubProvider).then((result) => {
+      const loggedUser = result.user;
+      console.log(loggedUser);
+      setUser(loggedUser);
+    });
+  };
+
+  //SignOut
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser(null);
+      })
+      .catch((error) => {
+        console.log("error", error.message);
+      });
+  };
+  return (
+    <div>
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <div className="w-full max-w-md bg-white rounded-lg shadow-md p-8">
+          <h2 className="lg:text-4xl md:text-4xl text-3xl font-bold text-center mb-6 text-sky-600">
+            Register Now
+          </h2>
+          {/* <h2 className="text-2xl font-bold text-center mb-6">
+            Email : {user ? user.displayName : "Guest"}
+          </h2> */}
+          <form onSubmit={handleRegister}>
+            <div className="space-y-4">
+              <div className="flex justify-center items-center gap-2">
+                <MdDriveFileRenameOutline className="text-2xl text-sky-600" />
+                <input
+                  id="name"
+                  type="name"
+                  name="name"
+                  required
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                  placeholder="Enter your name"
+                />
+              </div>
+              <div className="flex justify-center items-center gap-2">
+                <FaPhoneAlt className="text-2xl text-sky-600" />
+                <input
+                  id="number"
+                  type="number"
+                  name="number"
+                  required
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                  placeholder="Enter your number"
+                />
+              </div>
+              <div className="flex justify-center items-center gap-2">
+                <MdEmail className="text-2xl text-sky-600" />
+                <input
+                  id="email"
+                  type="email"
+                  name="email"
+                  required
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                  placeholder="Enter your email"
+                />
+              </div>
+              <div className="flex items-center gap-2">
+                <RiLockPasswordFill className="text-2xl text-sky-600" />
+                <div className="relative w-full">
+                  <input
+                    id="password"
+                    type={showPassword ? "text" : "password"}
+                    name="password"
+                    required
+                    className="w-full px-3 py-2 pr-10 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                    placeholder="Enter your password"
+                  />
+                  <span onClick={() => setShowPassword(!showPassword)}>
+                    {showPassword ? (
+                      <FaEyeSlash className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 cursor-pointer" />
+                    ) : (
+                      <FaEye className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 cursor-pointer" />
+                    )}
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="mt-6 space-y-4">
+              {user ? (
+                <div className="flex justify-center">
+                  <Button onClick={handleSignOut} variant="custom2">
+                    Signout
+                  </Button>
+                </div>
+              ) : (
+                <Button type="submit" variant="custom2">
+                  Sign Up
+                </Button>
+              )}
+
+              <div className="relative">
+                <div className="absolute inset-0 flex items-center">
+                  <div className="w-full border-t border-gray-300"></div>
+                </div>
+                <div className="relative flex justify-center text-sm">
+                  <span className="px-2 bg-white text-gray-500">Or</span>
+                </div>
+              </div>
+              <button
+                type="button"
+                onClick={handleGoogleSignin}
+                className="w-full gap-2 flex items-center justify-center bg-white hover:bg-gray-50 text-gray-700 font-bold py-2 px-4 rounded border border-gray-300 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
+              >
+                <FcGoogle className="lg:text-2xl text-xl" />
+                Sign up with Google
+              </button>
+              <button
+                type="button"
+                onClick={handleGithubSignin}
+                className="w-full gap-2 flex items-center justify-center bg-white hover:bg-gray-50 text-gray-700 font-bold py-2 px-4 rounded border border-gray-300 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
+              >
+                <FaGithub className="lg:text-2xl text-xl" />
+                Sign up with Github
+              </button>
+            </div>
+            <p className="py-4 text-lg text-center">
+              Already Have an account?{" "}
+              <a className="text-sky-600" href="/login">
+                SignIn
+              </a>{" "}
+            </p>
+          </form>
+          {registerError && (
+            <p className="text-red-600 text-center">{registerError}</p>
+          )}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Register;
